fadb9634d3b25c875157febe95b5750f3cea4ea4
Clinton Andrews <candrews800@gmail.com>
2015-06-11 12:05:48 -0400

    added generic info to work with vagrant/homestead

--- app/config/private.php
+++ app/config/private.php
@@ -2,5 +2,7 @@
 
     return array(
         'api_key' => 'pet_finder_api_key',
-        'google_api_key' => 'google_api_key'
+        'google_api_key' => 'google_api_key',
+        'adsense_client_key' => 'none',
+        'adsense_ad_slot' => 'none'
     );
