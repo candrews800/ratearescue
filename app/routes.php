ff9f42e6fa3b3a66c9705422257a37da9547b65a
Clinton Andrews <candrews800@gmail.com>
2015-06-24 10:04:54 -0400

    add github webhook

--- app/routes.php
+++ app/routes.php
@@ -14,3 +14,31 @@ Route::get('top/{rating_slug}/offset/{offset}', array('uses' => 'PetController@g
 Route::get('top/{rating_slug}/{pet_id}', array('uses' => 'PetController@showTop'));
 
 Route::get('pet/getStatus/{pet_id}', array('uses' => 'PetController@getStatus'));
+
+Route::post('pull_update', function(){
+
+    $private = require app_path().'/config/private.php';
+    $secret = $private['GITHUB_PULL_SECRET'];
+
+    $headers = getallheaders();
+    $hubSignature = $headers['X-Hub-Signature'];
+
+    // Split signature into algorithm and hash
+    list($algo, $hash) = explode('=', $hubSignature, 2);
+
+    // Get payload
+    $payload = file_get_contents('php://input');
+    $payload = json_decode($payload);
+    $payload = json_encode($payload);
+    $payload = str_replace('\/', '/', $payload);
+
+
+    // Calculate hash based on payload and the secret
+    $payloadHash = hash_hmac($algo, $payload, $secret);
+
+    if($hash === $payloadHash){
+        // execute git pull
+        return shell_exec("git pull");
+    }
+    return 'Wrong Hash';
+});
