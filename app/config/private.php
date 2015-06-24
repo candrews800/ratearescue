e4e2ffa8558a43a0863849bf30953aae7b1bc5b2
Clinton Andrews <candrews800@gmail.com>
2015-06-24 10:23:20 -0400

    test webhook

--- app/config/private.php
+++ app/config/private.php
@@ -4,5 +4,6 @@
         'api_key' => '9d442c7019aa349ec31ef2503c9333eb',
         'google_api_key' => 'google_api_key',
         'adsense_client_key' => 'none',
-        'adsense_ad_slot' => 'none'
+        'adsense_ad_slot' => 'none',
+        'GITHUB_PULL_SECRET' => 'review123'
     );
