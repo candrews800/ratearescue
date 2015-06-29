6fd3bad2b051859f8ae9821a2b8917f470d85ea5
Clinton Andrews <candrews800@gmail.com>
2015-06-28 23:19:36 -0400

    delete laravel4 files

--- app/config/private.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    return array(
-        'api_key' => '9d442c7019aa349ec31ef2503c9333eb',
-        'google_api_key' => 'google_api_key',
-        'adsense_client_key' => 'none',
-        'adsense_ad_slot' => 'none',
-        'GITHUB_PULL_SECRET' => 'review123'
-    );
